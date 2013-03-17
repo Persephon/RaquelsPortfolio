82e9497210ec865e24f43069558f3d4a815cd377
Kyle Merritt <kam03m@gmail.com>
2013-03-17 16:53:21 -0400

    first commit

---
+++ RaquelsPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace RaquelsPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Redirect("~/pages/home/home.html");
+        }
+
+    }
+}
